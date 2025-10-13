7aebbb5987b80ac95116a5613ec8f1fbd2145901
Joyboy28-cloud <kpalouyohan@gmail.com>
2025-10-13 14:10:59 +0000

    Ajout de la version avec referencement SEO

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -19,11 +19,26 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+
+from shop.sitemaps import ProductSitemap, CategorySitemap, StaticViewSitemap
+
+sitemaps = {
+    'products': ProductSitemap,
+    'categories': CategorySitemap,
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('',include('shop.urls')),
     path('administration/', include('shop.urls_admin')),
-    path('googlefd983fc4e0f7c2aa.html', TemplateView.as_view(template_name='googlefd983fc4e0f7c2aa.html')),
+    # SEO URLs
+    path('robots.txt', TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    )),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 if settings.DEBUG:
