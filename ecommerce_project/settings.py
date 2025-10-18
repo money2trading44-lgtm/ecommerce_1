33e4c6dfdf2c381c174fb479f52aaa20f91f57a5
Joyboy28-cloud <kpalouyohan@gmail.com>
2025-10-18 22:34:34 +0000

    Add products data

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
 
 import os
 from dotenv import load_dotenv
-import dj_database_url
+
+load_dotenv()
 
 
 MEDIA_URL = '/media/'
@@ -101,12 +102,22 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -161,7 +172,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configuration PayDunya (Wave, Orange, MTN)
-load_dotenv()  # Charge les variables d'environnement
+  # Charge les variables d'environnement
 
 # Configuration PayDunya
 CINETPAY_API_KEY = os.environ.get('CINETPAY_API_KEY', '')
