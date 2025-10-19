c8fb8ee43ffa42322da003d7312288203a246285
Joyboy28-cloud <kpalouyohan@gmail.com>
2025-10-19 16:53:13 +0000

    Test cloudinary

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -47,12 +47,7 @@ INSTALLED_APPS = [
     'users',
 ]
 
-# Configuration Cloudinary FORCÉE
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'debxh9hje'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
