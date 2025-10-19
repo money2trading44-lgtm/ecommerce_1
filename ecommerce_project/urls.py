fc1cd964924acdfedeabc10f47026016b427e670
Joyboy28-cloud <kpalouyohan@gmail.com>
2025-10-19 13:35:41 +0000

    Test cloudinary

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -1,13 +1,18 @@
+"""
+URL configuration for ecommerce_project project.
+"""
+
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse, HttpRequest
+
 
 def sitemap_xml(request):
     from django.urls import reverse
     from shop.models import Product
 
+    # URLs statiques
     urls = [
         'https://dsd-general-trading.com/',
         'https://dsd-general-trading.com/telephones/',
@@ -15,16 +20,20 @@ def sitemap_xml(request):
         'https://dsd-general-trading.com/reparation/',
     ]
 
+    # Ajouter les produits
     try:
         products = Product.objects.filter(is_active=True)
         for product in products:
             urls.append(f'https://dsd-general-trading.com/produit/{product.id}')
     except:
-        pass
+        pass  # En cas d'erreur DB, on garde les URLs statiques
 
+    # Générer le XML
     xml_content = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
     for url in urls:
         xml_content += f'  <url>\n    <loc>{url}</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.8</priority>\n  </url>\n'
+
     xml_content += '</urlset>'
 
     return HttpResponse(xml_content, content_type='application/xml')
@@ -45,11 +54,5 @@ urlpatterns = [
     path('administration/', include('shop.urls_admin')),
 ]
 
-# ⚠️ REMPLACEZ LA LIGNE CI-DESSOUS PAR CE BLOC :
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    from django.views.static import serve
-    urlpatterns += [
-        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+# ⚠️ SUPPRIMEZ TOUTE CONFIGURATION MEDIA ICI
+# Cloudinary gère automatiquement les URLs des images
